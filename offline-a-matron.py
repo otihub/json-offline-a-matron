3da0c4ee06b27eaad01e1d11bbfab188e006b8ef
notoncebut2x <rpnealon@gmail.com>
2016-07-08 16:21:52 -0400

    my first commit

---
+++ offline-a-matron.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import json
+
+filePath = raw_input("Paste the full path to the geoJSON you want to edit for offline use: ")
+varName = raw_input("What variable are you referencing this JSON as?: ")
+
+with open(filePath, "r+") as f:
+    data = json.load(f)
+    newData =("var " + varName + "=")
+    f.seek(0)
+    f.write(json.dumps(newData))
+    f.truncate()
+    f.write(json.dumps(data))
