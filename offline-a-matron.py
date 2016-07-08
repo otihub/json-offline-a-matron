d2665f84183e951555f774b3231ab2d94d2582b4
notoncebut2x <rpnealon@gmail.com>
2016-07-08 17:08:02 -0400

    Added comments to explain whats going on

--- offline-a-matron.py
+++ offline-a-matron.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+# import modules
 import json
 
+# get user input for file path and the variable name
 filePath = raw_input("Paste the full path to the geoJSON you want to edit for offline use: ")
 varName = raw_input("What variable are you referencing this JSON as?: ")
 
+# Read the JSON data and write the user input and original file back to it
 with open(filePath, "r+") as f:
     data = json.load(f)
     newData ="var " + varName + "="
@@ -12,4 +15,7 @@ with open(filePath, "r+") as f:
     f.write(newData)
     f.truncate()
     f.write(json.dumps(data))
+
+# Friendly Reminders
 print "Finished"
+print "Remember the var you are using in your js needs to match the var in the json!"
